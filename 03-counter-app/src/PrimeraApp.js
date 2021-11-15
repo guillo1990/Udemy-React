02744b193ec30fc56aa34f59afb7f4cdc473a5c3
Guillem <guillemgovi@gmail.com>
2021-11-15 22:40:26 +0100

    37-print variables in html

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -2,9 +2,14 @@ import { Fragment } from "react";
 
 const PrimeraApp = () => {
 
+    const str = {
+        nom: "Guillem",
+        edat: 31
+    };
+
     return (
         <>
-            <h1>Hello World</h1>
+            <h1>{JSON.stringify(str,null,3)}</h1>
             <p>My first application</p>
         </>        
     );
