92c6b4b3252e1bc8480abf76232ba86852924dfe
Guillem <guillemgovi@gmail.com>
2021-11-15 22:33:57 +0100

    36-Fragments

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,6 +1,13 @@
+import { Fragment } from "react";
+
 const PrimeraApp = () => {
 
-    return <h1>Hello World</h1>
+    return (
+        <>
+            <h1>Hello World</h1>
+            <p>My first application</p>
+        </>        
+    );
 
 }
 
